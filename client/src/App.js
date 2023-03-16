db2f0d1c3cbcebd19a4707f44f384aa924854720
Veasna <Veasna3d@gmail.com>
2023-03-16 22:45:47 +0700

    Add Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
+
+import Books from "./Pages/Books"
+import Add from "./Pages/Add"
+import Update from "./Pages/Update"
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books/>} />
+          <Route path="/add" element={<Add/>} />
+          <Route path="/update" element={<Update/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
